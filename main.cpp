7fe1375b3e1f913c3c3dc71b27e17a5269d5214e
jHat0421 <71831421+jHat0421@users.noreply.github.com>
2024-09-19 13:13:21 -0600

    Update main.cpp
    brought over from chessBoard.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-int main(){
-  cout << "Hello World!" << endl;
-  return 0;
-}
+int main() {
+    ChessBoard myBoard = ChessBoard();
+    myBoard.printBoard();
+    return 0;
+} 
